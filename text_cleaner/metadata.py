acf74914c10b8eb87b4970090f970922f9cb125a
huntzhan <programmer.zhx@gmail.com>
2016-07-23 18:39:08 +0800

    init.

---
+++ text_cleaner/metadata.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import (
+    division, absolute_import, print_function, unicode_literals,
+)
+
+
+NAME = 'text_cleaner'
+VERSION = '0.1.0'
+AUTHORS = [
+    'huntzhan',
+]
+EMAILS = [
+    'programmer.zhx@gmail.com',
+]
+LICENSE = ''
+URL = ''
+DESCRIPTION = 'purify text for NLP.'
